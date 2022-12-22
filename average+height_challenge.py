35c1872dcb159ed8ef7ea18bd77d638a22360afd
sudharaD <sudharad7@gmail.com>
2022-12-22 22:15:02 +0530

    temp

---
+++ average+height_challenge.py
@@ -0,0 +1,15 @@
+import random
+
+heights = []
+total_of_heights = 0
+
+for i in range(10):
+    height = random.uniform(135.0, 180.1)
+    heights.append(round(height, 1))
+
+for student_height in heights:
+    total_of_heights += student_height
+
+average_height = round(total_of_heights / 10, 2)
+
+print(f"Average height is {average_height}")
