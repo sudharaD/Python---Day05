18c030c12578ff241a71a6db80926846b74309d4
sudharaD <sudharad7@gmail.com>
2022-12-24 07:27:28 +0530

    best method

---
+++ adding_even_numbers.py
@@ -0,0 +1,15 @@
+total = 0
+
+for number in range(1, 101):
+    if number % 2 == 0:
+        total += number
+
+print(total)
+
+# best nethod
+total = 0
+
+for number in range(2, 101, 2):
+    total += number
+
+print(total)
