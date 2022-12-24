14bada9e7ad39bc57d36e11f4e10f5e0e5ea02a8
sudharaD <sudharad7@gmail.com>
2022-12-24 09:21:04 +0530

    fizz buzz completed edit 1

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,25 +1,29 @@
 for number in range(1, 101):
     if number % 3 != 0 and number % 5 != 0:
         print(number)
-    elif number % 3 == 0 and number % 5 == 0:
-        print("FizzBuzz")
-    elif number % 3 == 0:
-        print("Fizz")
-    elif number % 5 == 0:
-        print("Buzz")
+    # elif number % 3 == 0 and number % 5 == 0:
+    #     print("FizzBuzz")
+    # elif number % 3 == 0:
+    #     print("Fizz")
+    # elif number % 5 == 0:
+    #     print("Buzz")
     
     else:
         if number % 3 == 0:
             if number % 5 == 0:
-                print("Buzz")
+                print("FizzBuzz")
             else:
                 print("Fizz")
+        else:
+            print("Buzz")
 
-    if number % 3 == 0:
-            if number % 5 == 0:
-                print("Buzz")
-            else:
-                print("Fizz")
-    else:
-        print(number)
+    # if number % 3 == 0:
+    #     if number % 5 == 0:
+    #         print("FizzBuzz")
+    #     else:
+    #         print("Fizz")
+    # elif number % 5 == 0:
+    #     print("Buzz")
+    # else:
+    #     print(number)
     
